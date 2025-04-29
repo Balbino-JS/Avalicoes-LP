5aae3d42ce90b917bace5efe7075e0a0b011c787
Balbino-teste <balbinojteste@gmail.com>
2025-04-29 20:43:52 -0300

    stop

--- src/script.js
+++ src/script.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('formAvailacao');
     const lista = document.getElementById('lista-avaliacoes');
 
-;    //Carrega avaliações ao iniciar
+;  /*  //Carrega avaliações ao iniciar
     fetch('/api/avaliacoes')
     .then(res => res.json())
     .then(data => {
         data.forEach(addAvaliacaoNaTela);
-    });
+    });*/
+
+    
 
 });
